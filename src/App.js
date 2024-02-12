b5cf810733a1f27009b63318fa16bf44d8e959c6
Shantanu Badgujar <shantanubadgujar2362002@gmail.com>
2024-02-12 18:35:30 +0530

    almost complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css'
 import { NavbarSimple } from './components/NavBar';
+import { SimpleCard } from './components/cards';
+import { SimpleFooter } from './components/footer';
+
+
 function App() {
   return (
-    
-    <div className="">
-      <header className="">
-      <NavbarSimple className=""/>
-      </header>
-      
+  <div>
+    <div>
+        <header className="">
+         <NavbarSimple className=""/>
+        </header>
     </div>
+    <div className='px-6 py-7'>
+    <p className="mb-2">Browsing Category</p>
+    <h4 className='text-title text-uppercase text-black mt-3 mb-2'> Courses </h4>
+    </div>
+    <div className='cs-main-content'> 
+          <SimpleCard/>
+    </div>
+    <div>
+       <footer className=' flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between'>
+          <SimpleFooter className=''/>
+       </footer>
+    </div>
+  </div>
   );
 }
 
